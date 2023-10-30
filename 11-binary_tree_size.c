f0a4323aac510032516bbf9d89be2fba7f3a61b6
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-10-30 04:58:49 +0000

    created 12-binary_tree_leaves.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,14 +7,14 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-        size_t count = 0;
+	size_t count = 0;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        count += 1;
-        count += binary_tree_size(tree->left);
-        count += binary_tree_size(tree->right);
+	count += 1;
+	count += binary_tree_size(tree->left);
+	count += binary_tree_size(tree->right);
 
-        return (count);
+	return (count);
 }
---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,22 @@
+#include "binary_tree.h"
+/**
+* binary_tree_leaves - counts the number of leaves in the binary tree
+* @tree: pointer to the root node of the binary tree
+*
+* Return: number of leaves or 0 if unsuccessful
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree == NULL)
+		return (NULL);
+
+	if (tree->left == NULL && tree->right == NULL)
+		leaves += 1;
+
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
+
+	return (leaves);
+}
