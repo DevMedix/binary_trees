4aa8c78a72bddc0499137f59688efb5d58dc4d6a
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-10-30 04:27:20 +0000

    created 11-binary_tree_size.c

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+* binary_tree_size - measures the size of a binary tree
+* @tree: pointer to the root of the binary tree
+* Return: the size of tree or 0 if not successful
+*/
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+        size_t count = 0;
+
+        if (tree == NULL)
+                return (0);
+
+        count += 1;
+        count += binary_tree_size(tree->left);
+        count += binary_tree_size(tree->right);
+
+        return (count);
+}
