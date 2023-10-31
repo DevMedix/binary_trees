46b9f1adbee94fa95ba1d851a1cbed587d73584e
DevMedix <rawlingsmichael01@gmail.com>
2023-10-31 14:43:11 +0000

    new files

--- binary_trees.h
+++ binary_trees.h
@@ -5,8 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <bool.h>
-#include <tddef.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -30,5 +29,6 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREE_H_ */
